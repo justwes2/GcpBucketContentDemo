b9564c4f430ef7a81ffd3d856b575963799ec975
Wes <wes.coffay@gmail.com>
2018-09-02 23:03:54 -0400

    added on page video player, css

--- frontend/main.js
+++ frontend/main.js
@@ -7,7 +7,7 @@ $(function() {
        console.log(response);
        // $("#list").append('ahoy')
        for (let file of response) {
-         $("#list").append('<li>'+file["file name"]+' &nbsp;&nbsp;&nbsp; <a href='+file["link"]+'>link</a></li>');
+         $("#list").append('<li>' + file["file name"] + '&nbsp;&nbsp;&nbsp;<a onClick=playVideo("' +  file["link"].toString() + '")>play</a>&nbsp;&nbsp;&nbsp;<a href=' + file["link"] + '>link</a></li>');
        }
 
      },
@@ -16,3 +16,13 @@ $(function() {
      }
    });
 });
+
+
+function playVideo(link) {
+  const viewer = $("#viewer");
+  viewer.empty();
+  viewer.append('<video id="video" width="320" height="240" controls> <source src='+ link+' type="video/mp4"></video>')
+  const video = document.getElementById('video');
+  video.play()
+
+};
