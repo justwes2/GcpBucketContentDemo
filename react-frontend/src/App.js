e433722a3d207d55083f0bbe14feda8cf7ec57b7
Wes <wes.coffay@gmail.com>
2018-09-09 11:09:25 -0400

    starting react buildout

---
+++ react-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Player from './Player'
+
+class App extends Component {
+  render() {
+    return (
+      <Player />
+    );
+  }
+}
+
+export default App;
