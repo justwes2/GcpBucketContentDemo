02b4d43b48e4f760831edd22ca745a587440f894
Wes <wes.coffay@gmail.com>
2018-09-15 20:31:43 -0400

    error in list component

--- react-frontend/src/List.js
+++ react-frontend/src/List.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
 class List extends Component {
-  constructor(props){
-    super()
-  }
+
+  const videos = new Request("https://us-central1-serverless-mvp.cloudfunctions.net/showBucketContents", {
+    method: "POST",
+    "Content-Type": "application/json"
+  });
+  fetch(videos)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+  //   .then(data => {
+  //     // Loop over each user in the response and send it
+  //     // to the renderUser helper.
+  //     data.forEach(user => {
+  //       renderUser(user);
+  //     });
+  //   })
+  //   .catch(error => {
+  //     // If an error is found it will be caught here
+  //     // and can be subsequently handled.
+  //     console.log('Error Found:', error);
+  //   });
+  // });
   render() {
     return(
-    <h3>Hello world</h3>
+    // <h3>Hello world</h3>
     );
   }
 }
